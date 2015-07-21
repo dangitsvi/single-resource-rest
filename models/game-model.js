5ba367c62983c7a628a4f19616c69e45d8b70b74
dangitsvi <dangitsvi@gmail.com>
2015-07-21 09:43:50 -0700

    added data validation

--- models/game-model.js
+++ models/game-model.js
@@ -7,4 +7,8 @@ var gameSchema = mongoose.Schema({
   key: {type: String, unique: true}
 });
 
+gameSchema.path("rating").validate(function(v) {
+  return  v <= 10 && v > 0;
+}, 'rate on a scale of 1 - 10');
+
 module.exports = mongoose.model('Game', gameSchema);
